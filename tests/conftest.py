36e30948239c032568874d688c65277e6775c34c
danielandresbd-msmk <danielandresbd@msmk.university>
2026-06-30 00:17:59 +0200

    Fix random seed fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,13 +19,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 @pytest.fixture(autouse=True)
 def fijar_semilla_aleatoria():
 
-"""
-Esto inicializa el generador aleatorio con una semilla fija para que todos los
-tests trabajen con los mismos datos en cada ejecución. Esto hace que los
-tests sean repetibles y evita fallos aleatorios.
+    """
+    Esto inicializa el generador aleatorio con una semilla fija para que todos los
+    tests trabajen con los mismos datos en cada ejecución. Esto hace que los
+    tests sean repetibles y evita fallos aleatorios.
 
-autouse=True permite que este fixture se ejecute automáticamente.
-"""
-
-
+    autouse=True permite que este fixture se ejecute automáticamente.
+    """
     random.seed(42)
